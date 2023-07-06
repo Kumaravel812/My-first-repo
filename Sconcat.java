5d6577f09d6047e6b823716a49757446546488e5
KUMAREAVEL VEERASAMY <kumaravelsurya812@gmail.com>
2023-07-06 14:49:49 +0530

    Sconcat Upload

---
+++ Sconcat.java
@@ -0,0 +1,14 @@
+public class Sconcat
+{
+	public static void main(String... args)
+	{
+		String k1 = "2";
+		String k2 = "3";
+
+		String sum = k1 + k2;
+
+		System.out.println("k1 : " + k1);
+		System.out.println("k2 : " + k2);
+		System.out.println("Sum : " + sum);
+	}
+}
